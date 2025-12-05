bfa65bf62313dcfad6990780004e258c23db2a44
José Augusto <joseaugusto0408@gmail.com>
2025-12-05 06:14:45 -0300

    headers

--- src/server.js
+++ src/server.js
@@ -1,14 +1,22 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
-
   if (method === "GET" && url === "/users") {
-    return res.end("Listagem de usuários");
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
-    return res.end("Criação de usuário");
+    users.push({
+      id: Date.now(),
+      name: "José Augusto",
+      email: "joseaugusto0408@gmail.com",
+    });
+    return res.end(`Criação de usuário`);
   }
   return res.end("Hello World - Rocketseat!!");
 });
