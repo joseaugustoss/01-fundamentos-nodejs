b003d2d164dd3d95fa58d76c82a0de5b94be9673
José Augusto <joseaugusto0408@gmail.com>
2025-12-12 05:37:38 -0300

    stream completo

--- streams/fake-upload-to-http-stream.js
+++ streams/fake-upload-to-http-stream.js
@@ -5,7 +5,7 @@ class OneToHundredStream extends Readable {
   _read() {
     const i = this.index++;
     setTimeout(() => {
-      if (i > 100) {
+      if (i > 5) {
         this.push(null);
       } else {
         const buf = Buffer.from(String(i));
@@ -18,4 +18,8 @@ fetch('http://localhost:3334', {
   method: 'POST',
   duplex: 'half',
   body: new OneToHundredStream()
+}).then(response => {
+  return response.text()
+}).then(data => {
+  console.log(data)
 })
