3100a386b6c1cb27ff20d74afe1b055b00c7510b
José Augusto <joseaugusto0408@gmail.com>
2025-12-15 08:58:28 -0300

    JSON (stream & buffers)

--- src/server.js
+++ src/server.js
@@ -2,8 +2,19 @@ import http from "node:http";
 
 const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
   if (method === "GET" && url === "/users") {
     return res
       .setHeader("Content-type", "application/json")
@@ -11,10 +22,11 @@ const server = http.createServer((req, res) => {
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
     users.push({
       id: Date.now(),
-      name: "José Augusto",
-      email: "joseaugusto0408@gmail.com",
+      name,
+      email,
     });
     return res.writeHead(201).end();
   }
