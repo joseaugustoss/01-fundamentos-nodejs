0483d613f7b7e3024eac27fdb3b70288703827f4
José Augusto <joseaugusto0408@gmail.com>
2025-12-16 11:13:06 -0300

    uuid

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
+import { Database } from "./database.js";
+import { randomUUID } from "node:crypto";
 
-const users = [];
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -9,16 +11,19 @@ const server = http.createServer(async (req, res) => {
   await json(req, res);
 
   if (method === "GET" && url === "/users") {
+    const users = database.select("users");
     return res.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
     const { name, email } = req.body;
-    users.push({
-      id: Date.now(),
+    const user = {
+      id: randomUUID(),
       name,
       email,
-    });
+    };
+
+    database.insert("users", user);
     return res.writeHead(201).end();
   }
   return res.writeHead(401).end();
